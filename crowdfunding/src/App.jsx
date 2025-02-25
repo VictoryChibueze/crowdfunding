91765afe88fe7af2951469157890cfd4381cca8d
Dr.Zeus⚡ <144681284+VictoryChibueze@users.noreply.github.com>
2025-02-25 19:08:25 +0100

    crowdfunding page

--- crowdfunding/src/App.jsx
+++ crowdfunding/src/App.jsx
@@ -1,6 +1,7 @@
+import { TbBrandMcdonalds } from "react-icons/tb";
 function HomePage() {
   return (
-    <main className="">
+    <main className="bg-[#f2f2f2] h-200">
       <nav className="flex justify-around text-white absolute inset-x-0 z-100 py-3 bg-[#636363e6]">
         <h3 className="text-base font-bold cursor-pointer text-lg">
           CrowdFund
@@ -27,8 +28,15 @@ function HomePage() {
           width="1500"
         />
       </picture>
-      <div>
-        <h2>MasterCraft Bamboo Monitor Riser</h2>
+      <div className="md:w-[50%] bg-[#ffff] w-[90%] my-0 mx-auto px-2 rounded-lg">
+        <TbBrandMcdonalds className="bg-gray-700 text-white h-7 w-7 rounded-full p-1 mx-auto " />
+
+        <h2 className="text-center text-xl my-0 font-bold">
+          MasterCraft Bamboo Monitor Riser
+        </h2>
+        <p className="text-center text-[#555] text-sm">
+          A beautiful handcrafted monitor stand to reduce neck and eye strain
+        </p>
       </div>
     </main>
   );
